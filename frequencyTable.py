84e11c96cc81c07b14cbfaa5d0f46cca6c93b800
Anthony Chen <pandaman0529@gmail.com>
2014-02-12 11:55:42 -0500

    frequencyTable generation for unigram

---
+++ frequencyTable.py
@@ -0,0 +1,9 @@
+def create_frequency_table(parsed_list):
+    frequencyTable = {}
+    for token_list in parsed_list:
+        for token in token_list:
+            if token in frequencyTable:
+                frequencyTable[token] += 1
+            else:
+                frequencyTable[token] = 1
+    return frequencyTable
