f38905ea814962144ccf1e0c516d6f18073f9953
Anthony Chen <pandaman0529@gmail.com>
2014-02-12 11:54:02 -0500

    Conversion to soft tabs

--- probabilityTable.py
+++ probabilityTable.py
@@ -1,27 +1,27 @@
 def createProbabilityTable(frequencyTable):
-	counter = 0.0
-	probabilityTable = {}
-	for token in frequencyTable:
-		counter += frequencyTable[token]
-	for token in frequencyTable:
-		probabilityTable[token] = frequencyTable[token]/counter
-	return probabilityTable	
+    counter = 0.0
+    probabilityTable = {}
+    for token in frequencyTable:
+        counter += frequencyTable[token]
+    for token in frequencyTable:
+        probabilityTable[token] = frequencyTable[token]/counter
+    return probabilityTable 
 
 
 
 def createCumulativeTable(probabilityTable):
-	probatility = 0.0
-	counter = 0
-	cumulativeTable = {}
-	for token in probabilityTable:
-		counter += 1
-		if counter != len(probabilityTable):
-			probatility += probabilityTable[token]
-			cumulativeTable[token] = probatility
-		else:
-			cumulativeTable[token] = 1.0
+    probatility = 0.0
+    counter = 0
+    cumulativeTable = {}
+    for token in probabilityTable:
+        counter += 1
+        if counter != len(probabilityTable):
+            probatility += probabilityTable[token]
+            cumulativeTable[token] = probatility
+        else:
+            cumulativeTable[token] = 1.0
 
-	return cumulativeTable
+    return cumulativeTable
 
 frequencyTable = {'a':1, 'b':2, 'c':3, 'd':4}
 probabilityTable = createProbabilityTable(frequencyTable)
