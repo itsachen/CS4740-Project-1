0938da5151c755d72b9a2da38a46042bda4de152
Anthony Chen <pandaman0529@gmail.com>
2014-02-11 20:27:54 -0500

    xmlParser soft tabs

--- xmlParser.py
+++ xmlParser.py
@@ -1,21 +1,21 @@
 import xml.etree.ElementTree as ET
 
 def parseXml(file):
-	tree = ET.parse(file)
-	root = tree.getroot()
+  tree = ET.parse(file)
+  root = tree.getroot()
 
-	sentenceList = []
+  sentenceList = []
 
-	for doc in root:
-		for text in doc:
-			words = text.text.split()
-			#print words
-			wordList = []
-			for word in words:
-				wordList.append(word)
-				if "." in word:
-					sentenceList.append(wordList)
-					wordList = []
-	return sentenceList
+  for doc in root:
+    for text in doc:
+      words = text.text.split()
+      #print words
+      wordList = []
+      for word in words:
+        wordList.append(word)
+        if "." in word:
+          sentenceList.append(wordList)
+          wordList = []
+  return sentenceList
 
 print parseXml("kjbible.train")
