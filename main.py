fe7e737b6fce98bdb009507ed5a46770e2127c24
James Feher <jkf49@cornell.edu>
2014-02-18 14:28:26 -0500

    Sentence on one line. Adjust spacing for punctuation?

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ outList = parse_hotel_reviews()
 cumulativeTable = createCumulativeTable(createProbabilityTable(create_frequency_table(outList)))
 #print cumulativeTable
 token = '<s>'
+s = ""
 while token != '<e>' :
     r = random.random()
     for (token, probability) in cumulativeTable :
         if r < probability :
             if not(token == '<e>' or token == '<s>') :
-                print token
+                if s != "":
+                    s+= " "
+                s += token
             break
+print s
