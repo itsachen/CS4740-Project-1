9d4b7a7ef0d50856ff507560ff523d6f5be622d3
James Feher <jkf49@cornell.edu>
2014-02-19 20:46:06 -0500

    Bigram and unigram sentence generator mostly completed

--- main.py
+++ main.py
@@ -2,18 +2,46 @@ from Parser import *
 from probabilityTable import *
 from frequencyTable import *
 import random
-outList = parse_hotel_reviews()
-cumulativeTable = createCumulativeTable(createProbabilityTable(create_frequency_table(outList)))
-#print cumulativeTable
-token = '<s>'
-s = ""
-while token != '<e>' :
-    r = random.random()
-    for (token, probability) in cumulativeTable :
-        if r < probability :
-            if not(token == '<e>' or token == '<s>') :
-                if s != "":
-                    s+= " "
-                s += token
-            break
-print s
+
+#hotel and unigram are boolean, n is the number of sentences
+def write_sentences(hotel, unigram, n):
+    if hotel :
+        outList = parse_hotel_reviews()
+    else :
+        outList = parse_bible()
+    if unigram :
+        for i in range(n):
+            cumulativeTable = createCumulativeTable(createProbabilityTable(create_frequency_table(outList)))
+            #print cumulativeTable
+            token = '<s>'
+            s = ""
+            while token != '<e>' :
+                r = random.random()
+                for (token, probability) in cumulativeTable :
+                    if r < probability :
+                        if not(token == '<e>' or token == '<s>') :
+                            if s != "":
+                                s+= " "
+                            s += token
+                        break
+            print s + "\n"
+    else :
+        for i in range(n):
+            #TODO: bigramify
+            cumulativeTable = createCumulativeTable(createProbabilityTable(create_frequency_table(outList)))
+            
+            #print cumulativeTable
+            token = '<s>'
+            s = ""
+            while token != '<e>' :
+                r = random.random()
+                for (token, probability) in cumulativeTable[token] :
+                    if r < probability :
+                        if not(token == '<e>' or token == '<s>') :
+                            if s != "":
+                                s+= " "
+                            s += token
+                        break
+            print s + "\n"
+        
+write_sentences(True, True, 10)
