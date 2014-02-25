814c2b33a14d542113998def2cda12de4c09b141
Anthony Chen <pandaman0529@gmail.com>
2014-02-24 22:19:57 -0500

    Hotel parsing separates truthful and untruthful token lists

--- Parser.py
+++ Parser.py
@@ -7,7 +7,7 @@ hotel_dataset = "HotelReviews/reviews.train"
 bible_dataset = "bible_corpus/kjbible.train"
 
 # TODO: Replace Windows 1252 characters for readability
-def parse_hotel_reviews(filename=hotel_dataset):
+def parse_all_hotel_reviews(filename=hotel_dataset):
     output_list = []
 
     with open(filename) as f:
@@ -23,7 +23,26 @@ def parse_hotel_reviews(filename=hotel_dataset):
 
     return output_list
 
-# TODO: Parse passage notation? ie. 10:4 as three tokens
+def parse_hotel_reviews_for_truthfulness(filename=hotel_dataset):
+    truthful_list = []
+    untruthful_list = [] 
+
+    with open(filename) as f:
+        next(f) # Skip the first line that describes the columns
+        for line in f:
+            review_text = line[4:]
+            sentences = sentence_tokenizer.tokenize(review_text)
+            for sentence in sentences:
+                tokenized_sentence = nltk.word_tokenize(sentence)
+                tokenized_sentence.insert(0,'<s>')
+                tokenized_sentence.append('<e>')
+                if line[0] == 1:
+                    truthful_list.append(tokenized_sentence)
+                else:
+                    untruthful_list.append(tokenized_sentence)
+
+    return {"truthful": truthful_list, "untruthful": untruthful_list}
+
 def parse_bible(filename=bible_dataset):
     output_list = []
     filetext = open(filename).read()
