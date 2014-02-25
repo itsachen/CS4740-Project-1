c9109d482b3e6b98d80ccad50f769ce9682e3760
James Feher <jkf49@cornell.edu>
2014-02-24 23:21:55 -0500

    Frequency Table has unknown words now

--- frequencyTable.py
+++ frequencyTable.py
@@ -1,7 +1,11 @@
 def create_unigram_frequency_table(parsed_list):
     frequencyTable = {}
+    wordSet = set()
     for token_list in parsed_list:
         for token in token_list:
+            if(token not in wordSet):
+                wordSet.add(token)
+                token = 'UNK'
             if token in frequencyTable:
                 frequencyTable[token] += 1
             else:
@@ -10,12 +14,19 @@ def create_unigram_frequency_table(parsed_list):
 
 def create_bigram_frequency_table(parsed_list):
     frequencyTable = {}
+    wordSet = set()
     for token_list in parsed_list:
         token_list_length = len(token_list)
         for i in range(token_list_length):
             if i < token_list_length - 1:
                 current_word = token_list[i]
                 next_word = token_list[i+1]
+                if current_word not in wordSet:
+                    wordSet.add(current_word)
+                    current_word = 'UNK'
+                if next_word not in wordSet :
+                    wordSet.add(next_word)
+                    next_word = 'UNK'
                 if current_word in frequencyTable:
                     if next_word in frequencyTable[current_word]:
                         frequencyTable[current_word][next_word] += 1
