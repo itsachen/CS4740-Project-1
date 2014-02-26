a6e236dcea766a288336d76fa9cced6bdd3b81fc
James Feher <jkf49@cornell.edu>
2014-02-26 07:30:02 -0500

    Unknown word handling added. Score increased to .5666667. Treats words that appear only once in training set as UNK

--- frequencyTable.py
+++ frequencyTable.py
@@ -1,9 +1,9 @@
-def create_unigram_frequency_table(parsed_list):
+def create_unigram_frequency_table(parsed_list, unk):
     frequencyTable = {}
     wordSet = set()
     for token_list in parsed_list:
         for token in token_list:
-            if(token not in wordSet):
+            if unk and token not in wordSet:
                 wordSet.add(token)
                 token = 'UNK'
             if token in frequencyTable:
@@ -12,20 +12,25 @@ def create_unigram_frequency_table(parsed_list):
                 frequencyTable[token] = 1
     return frequencyTable
 
-def create_bigram_frequency_table(parsed_list):
+def create_bigram_frequency_table(parsed_list, unk):
     frequencyTable = {}
     wordSet = set()
+    unigramFreqTable = {}
+    if unk :
+        unigramFreqTable = create_unigram_frequency_table(parsed_list, False)
+        for token in unigramFreqTable :
+            if unigramFreqTable[token] == 1 :
+                wordSet.add(token)
+    
     for token_list in parsed_list:
         token_list_length = len(token_list)
         for i in range(token_list_length):
             if i < token_list_length - 1:
                 current_word = token_list[i]
                 next_word = token_list[i+1]
-                if current_word not in wordSet:
-                    wordSet.add(current_word)
+                if unk and current_word in wordSet:
                     current_word = 'UNK'
-                if next_word not in wordSet :
-                    wordSet.add(next_word)
+                if unk and next_word in wordSet :
                     next_word = 'UNK'
                 if current_word in frequencyTable:
                     if next_word in frequencyTable[current_word]:
@@ -34,5 +39,32 @@ def create_bigram_frequency_table(parsed_list):
                         frequencyTable[current_word][next_word] = 1
                 else:
                     frequencyTable[current_word] = {next_word:1}
-    return frequencyTable
+    return frequencyTable, wordSet
 
+#Returns the new frequency table and an "unknown" probability
+def smooth_bigram_frequency_table(frequencyTable):
+    counts = {}
+    totalBigrams = 0
+    #Get counts for N1, N2, etc.
+    for token in frequencyTable :
+        for token2 in frequencyTable[token] :
+            freq = frequencyTable[token][token2]
+            if frequencyTable[token][token2] in counts :
+                counts[freq] += 1
+                totalBigrams += 1
+            else :
+                counts[freq] = 1
+                totalBigrams += 1
+    #Determine what frequency counts to smooth
+    i = 1
+    while i in counts :
+        maxCount = i-1
+        i += 1
+    
+    #Smooth frequencies
+    for token in frequencyTable :
+        for token2 in frequencyTable[token] :
+            freq = frequencyTable[token][token2]
+            if freq < maxCount :
+                frequencyTable[token][token2] = float(freq + 1) * counts[freq + 1] / counts[freq]
+    return frequencyTable, float(counts[1]) / totalBigrams
--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import sys
 #hotel and unigram are boolean, n is the number of sentences
 def write_sentences(hotel, unigram, n):
     if hotel :
-        outList = parse_hotel_reviews()
+        outList = parse_all_hotel_reviews()
     else :
         outList = parse_bible()
     if unigram :
         for i in range(n):
-            cumulativeTable = createCumulativeTable(createProbabilityTable(create_unigram_frequency_table(outList)))
+            cumulativeTable = createCumulativeTable(createProbabilityTable(create_unigram_frequency_table(outList, False)))
             #print cumulativeTable
             token = '<s>'
             s = ""
@@ -29,7 +29,8 @@ def write_sentences(hotel, unigram, n):
     else :
         for i in range(n):
             #TODO: bigramify
-            cumulativeTable = createBigramCumulativeTable(createBigramProbabilityTable(create_bigram_frequency_table(outList)))
+            frequencyTable, wordSet = create_bigram_frequency_table(outList, False)
+            cumulativeTable = createBigramCumulativeTable(createBigramProbabilityTable(frequencyTable))
 
             token = '<s>'
             s = ""
@@ -46,19 +47,26 @@ def write_sentences(hotel, unigram, n):
 #arg1 = hotel, arg2 = unigram, arg3 = number of sentences      
 #write_sentences(sys.argv[1] == 'True', sys.argv[2] == 'True', int(sys.argv[3]))
 
+def create_smoothed_bigram_probability_table(outList):
+    frequencyTable, wordSet = create_bigram_frequency_table(outList, True)
+    frequencyTable, prob = smooth_bigram_frequency_table(frequencyTable)
+    probabilityTable = createBigramProbabilityTable(frequencyTable)
+    return probabilityTable, prob, wordSet
+
 def predict_sentence_list():
     output_list = []
     train_sentence_list = parse_hotel_reviews_for_truthfulness()
     test_sentence_list = parse_kaggle_hotel_reviews("HotelReviews/kaggle_data_file.txt")
 
-    truthful_bigram = createBigramProbabilityTable(create_bigram_frequency_table(train_sentence_list["truthful"]))
-    untruthful_bigram = createBigramProbabilityTable(create_bigram_frequency_table(train_sentence_list["untruthful"]))
+    truthful_bigram, probUnkTruthful, wordSetTruthful = create_smoothed_bigram_probability_table(train_sentence_list["truthful"])
+    untruthful_bigram, probUnkUntruthful, wordSetUntruthful = create_smoothed_bigram_probability_table(train_sentence_list["untruthful"])
     counter = 0
+
     for review in test_sentence_list:
         probability_truthful = 0.0
         probability_untruthful = 0.0
         for tokenList in review:
-            truthful_tuple = isTruthful(tokenList, truthful_bigram, untruthful_bigram)
+            truthful_tuple = isTruthful(tokenList, truthful_bigram, untruthful_bigram, probUnkTruthful, probUnkUntruthful, wordSetTruthful, wordSetUntruthful)
             probability_truthful = probability_truthful + truthful_tuple[0]
             probability_untruthful = probability_untruthful + truthful_tuple[1]
         if probability_truthful >= probability_untruthful:
@@ -69,7 +77,7 @@ def predict_sentence_list():
     return output_list
 
 
-def isTruthful(tokenList, truthful_bigram, untruthful_bigram):
+def isTruthful(tokenList, truthful_bigram, untruthful_bigram, probUnkTruthful, probUnkUntruthful, truthfulWordList, untruthfulWordList):
     probability_truthful = 1.0
     probability_untruthful = 1.0
     previous_token = None
@@ -77,12 +85,29 @@ def isTruthful(tokenList, truthful_bigram, untruthful_bigram):
         if previous_token is None:
             previous_token = token
         else:
-            if previous_token in truthful_bigram:
-                if token in truthful_bigram[previous_token]:
-                    probability_truthful = probability_truthful * truthful_bigram[previous_token][token]
-            if previous_token in untruthful_bigram:
-                if token in untruthful_bigram[previous_token]:
-                    probability_untruthful = probability_untruthful * untruthful_bigram[previous_token][token]
+            tokenT = token
+            previous_tokenT = previous_token
+            tokenUT = token
+            previous_tokenUT = previous_token
+            
+            if token not in truthfulWordList:
+                tokenT = '<UNK>'
+            if previous_token not in truthfulWordList:
+                previous_tokenT = '<UNK>'
+            if token not in untruthfulWordList:
+                tokenUT = '<UNK>'
+            if previous_token not in untruthfulWordList:
+                previous_tokenUT = '<UNK>'
+            
+            
+            if previous_tokenT in truthful_bigram and tokenT in truthful_bigram[previous_tokenT]:
+                probability_truthful = probability_truthful * truthful_bigram[previous_tokenT][tokenT]
+            else :
+                probability_truthful = probability_truthful * probUnkTruthful
+            if previous_tokenUT in untruthful_bigram and tokenUT in untruthful_bigram[previous_tokenUT]:
+                probability_untruthful = probability_untruthful * untruthful_bigram[previous_tokenUT][tokenUT]
+            else:
+                probability_truthful = probability_truthful * probUnkUntruthful
             previous_token = token
     return [probability_truthful, probability_untruthful]
 
